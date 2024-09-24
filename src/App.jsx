920be3901121dc0af439aa9c2db9350da2aec4d3
Lucas Capdevila <lcapdevila60@gmail.com>
2024-09-23 22:21:18 -0300

    se inicia con página Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import AppRouter from "./components/routes/AppRouter"
+
 function App() {
 
   return (
     <>
-      
+      <AppRouter />
     </>
   )
 }
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <main className="h-screen">
+      <div className="flex h-full">
+        <div className="w-2/5 bg-slate-600"></div>
+        <div className="w-3/5 bg-slate-950"></div>
+      </div>
+    </main>
+  )
+}
+
+export default Home
