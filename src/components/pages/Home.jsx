ef6c283f020f564f220ecd1dad992e4f811ac37c
Lucas Capdevila <lcapdevila60@gmail.com>
2024-09-24 23:05:36 -0300

    se realiza responsive (tablet)

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,22 +6,23 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <main className="h-screen">
-      <div className="relative flex flex-col xl:flex-row h-full">
-        <div className="w-full xl:w-3/5 h-2/5 xl:h-full bg-reactLightBlue"></div>
+      <div className="relative flex flex-col md:flex-row h-full">
+        <div className="w-full md:w-1/2 xl:w-3/5 h-2/5 md:h-full bg-reactLightBlue"></div>
         <img
           src={imagen}
-          className="absolute inset-0 size-64 xl:size-96 rounded-full border-8 border-whiteBg md:border-none object-cover mx-auto my-auto transform -translate-x-0 -translate-y-1/3 md:translate-x-0 md:-translate-y-1/2"
+          className="absolute inset-0 size-64 md:size-80 xl:size-96 rounded-full border-8 border-whiteBg md:border-none object-cover mx-auto my-auto transform -translate-x-0 -translate-y-1/3 md:-translate-x-1/3 md:-translate-y-1"
           alt="lucasecapdevila" />
-        <div className="w-full xl:w-3/5 flex flex-col justify-center items-center mt-36 bg-whiteBg">
-          <div className="flex flex-col-reverse justify-center items-center">
+        <div className="w-full md:w-1/2 xl:w-3/5 flex flex-col justify-center items-center md:items-start mt-36 md:mt-0 bg-whiteBg">
+          <div className="flex flex-col-reverse justify-center items-center md:items-start md:ms-20">
             <h1 className="text-textBlack text-4xl font-bold">Lucas Capdevila</h1>
             <h2 className="text-textBlack text-2xl font-bold">Fullstack Developer</h2>
           </div>
-          <p className="text-textBlack text-lg text-center font-normal content-end mt-4">Soy un apasionado desarrollador web especializado en Stack MERN con experiencia creando aplicaciones web con React.js, Node/Express y MongoDB. Estoy muy interesado en aprender nuevas tecnologías para implementarlas en mis proyectos.</p>
+          <p className="text-textBlack text-lg text-center md:text-left font-normal content-end mt-4 md:ms-20 md:mb-20">Soy un apasionado desarrollador web especializado en Stack MERN con experiencia creando aplicaciones web con React.js, Node/Express y MongoDB. Estoy muy interesado en aprender nuevas tecnologías para implementarlas en mis proyectos.</p>
+          <a href={pdf} download className="w-full md:w-2/3 md:ms-16 px-4 mt-4 justify-center">
+            <Button pill outline className="cvBtn w-full border-4 border-reactLightBlue p-0 text-reactLightBlue font-bold">Descargar CV</Button>
+          </a>
+          <Button as={Link} to="/contacto" pill className="contactoBtn w-full md:w-3/5 md:ms-20 md:px-4 p-0 mt-4 bg-reactLightBlue border-4 text-textBlack font-bold">Contacto</Button>
         </div>
-        <a href={pdf} download className="mx-4 mt-4 justify-center">
-          <Button pill outline className="cvBtn w-full bg-black border-4 border-reactLightBlue p-0 text-reactLightBlue font-bold">Descargar CV</Button>
-        </a>
       </div>
     </main>
   )
