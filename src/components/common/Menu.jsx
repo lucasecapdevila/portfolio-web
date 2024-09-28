66d20f1a8c123e501acf92cbe19af723a42645b0
Lucas Capdevila <lcapdevila60@gmail.com>
2024-09-27 23:03:04 -0300

    sidebar en mobile terminado

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faFlag, faFolder, faHouseChimney, faMoon, faPhone, faScrewdriverWrench, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import logo from "../../assets/logo-black2.png"
 import { Link } from "react-router-dom";
-import { faFlag, faFolder, faHouseChimney, faMoon, faPhone, faScrewdriverWrench, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Menu = () => {
   return (
@@ -44,73 +45,95 @@ const Menu = () => {
 
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 mt-10 w-64 h-screen pt-20 transition-transform -translate-x-full bg-reactLightBlue sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        className="enlaces fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-reactLightBlue sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 pb-4 overflow-y-auto">
-          <ul className="space-y-2 font-medium text-lg">
+        <div className="h-full flex flex-col justify-between px-3 pb-4 overflow-y-auto">
+          <ul className="mt-6 space-y-2 font-medium text-lg">
             <li>
               <Link
                 to="/"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faHouseChimney} />
+                <FontAwesomeIcon icon={faHouseChimney} className="text-2xl" />
                 <span className="ms-3">Inicio</span>
               </Link>
             </li>
             <li>
               <Link
                 to="/about"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faUser} />
+                <FontAwesomeIcon icon={faUser} className="text-2xl" />
                 <span className="ms-4">Sobre mí</span>
               </Link>
             </li>
             <li>
               <Link
                 to="/skills"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faScrewdriverWrench} />
+                <FontAwesomeIcon icon={faScrewdriverWrench} className="text-2xl" />
                 <span className="ms-4">Habilidades</span>
               </Link>
             </li>
             <li>
               <Link
-                to="/skills"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                to="/projects"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faFolder} />
+                <FontAwesomeIcon icon={faFolder} className="text-2xl" />
                 <span className="ms-4">Proyectos</span>
               </Link>
             </li>
             <li>
               <Link
-                to="/skills"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                to="/contact"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faPhone} />
+                <FontAwesomeIcon icon={faPhone} className="text-2xl" />
                 <span className="ms-4">Contacto</span>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 to="/"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon icon={faFlag} />
+                <FontAwesomeIcon icon={faFlag} className="text-2xl" />
                 <span className="ms-4">EN/ES</span>
               </Link>
             </li>
             <li>
               <Link
                 to="/"
-                className="flex items-center p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className="flex items-center text-xl p-2 text-textBlack rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FontAwesomeIcon className=" me-4" icon={faSun} />
-                <FontAwesomeIcon icon={faMoon} />
+                <FontAwesomeIcon className="text-2xl me-4" icon={faSun} />
+                <FontAwesomeIcon icon={faMoon} className="text-2xl" />
                 <span className="ms-4"></span>
+              </Link>
+            </li> */}
+          </ul>
+          <ul className="mb-2 md:columns-2 space-y-4 font-medium text-lg">
+            <li className="px-2">
+              <Link to='mailto:example@example.com?subject=FullStack Portfolio&body=Hola ! Vi tu portfolio y me interesan tus servicios.'>
+                <FontAwesomeIcon icon={faEnvelope} className="text-3xl" />
+              </Link>
+            </li>
+            <li className="px-2">
+              <Link to='https://www.linkedin.com/in/lucasecapdevila/' target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} className="text-3xl" />
+              </Link>
+            </li>
+            <li className="px-2">
+              <Link to='https://github.com/lucasecapdevila' target="_blank">
+                <FontAwesomeIcon icon={faGithub} className="text-3xl" />
+              </Link>
+            </li>
+            <li className="px-2">
+              <Link to='https://www.instagram.com/lucasecapdevila/' target="_blank">
+                <FontAwesomeIcon icon={faInstagram} className="text-3xl" />
               </Link>
             </li>
           </ul>
