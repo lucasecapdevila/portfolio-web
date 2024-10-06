0e5aa35bacc28253563bad8018e32b95d99b5685
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-06 20:24:30 -0300

    se agrega función para mostrar sideBar en Vercel

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Menu from "../common/Menu"
 
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
