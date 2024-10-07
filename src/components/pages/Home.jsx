52756cf96be9baf60d899ce54197ffc1bc18136c
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-06 21:59:51 -0300

    se ajusta página de inicio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
         <div className="w-full md:w-1/2 xl:w-2/5 h-2/5 md:h-full md:bg-reactLightBlue"></div>
         <img
           src={imagen}
-          className="absolute inset-0 size-64 md:size-80 xl:size-96 rounded-full object-cover mx-auto my-auto transform -translate-x-0 -translate-y-3/4 md:-translate-x-1/3 md:-translate-y-1 xl:-translate-x-1/2"
+          className="absolute inset-0 size-60 md:size-80 xl:size-96 rounded-full object-cover mx-auto my-auto transform -translate-x-0 -translate-y-3/4 md:-translate-x-1/3 md:-translate-y-1 xl:-translate-x-1/2"
           alt="lucasecapdevila" />
-        <div className="w-full md:w-1/2 xl:w-3/5 flex flex-col justify-center items-center md:items-start mt-10 md:mt-0 bg-whiteBg">
+        <div className="w-full md:w-1/2 xl:w-3/5 flex flex-col justify-center items-center md:items-start mt-8 md:mt-0 bg-whiteBg">
           <div className="flex flex-col-reverse justify-center items-center md:items-start md:ms-20 xl:ms-72">
             <h1 className="text-textBlack text-4xl font-bold">Lucas Capdevila</h1>
             <h2 className="text-textBlack text-2xl font-bold">Fullstack Developer</h2>
