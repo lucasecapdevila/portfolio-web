641d8d8cc89bb7889e500dd296bec7af4ae46e1d
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-07 08:46:18 -0300

    se agrega hover a botón de Descargar CV

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
           alt="lucasecapdevila" />
         <div className="w-full md:w-1/2 xl:w-3/5 flex flex-col justify-center items-center md:items-start mt-36 md:mt-0 bg-whiteBg">
           <div className="flex flex-col-reverse justify-center items-center md:items-start md:ms-20 xl:ms-72">
-            <h1 className="text-textBlack text-3xl font-bold">Lucas Capdevila</h1>
+            <h1 className="text-textBlack text-3xl md:text-4xl font-bold">Lucas Capdevila</h1>
             <h2 className="text-textBlack text-2xl font-bold">Fullstack Developer</h2>
           </div>
-          <p className="text-textBlack text-lg text-center md:text-left font-normal content-end px-2 md:ps-0 md:pe-2 mt-4 md:ms-20 md:mb-20 xl:ms-72 xl:mb-20">Soy un apasionado desarrollador web especializado en Stack MERN con experiencia creando aplicaciones web con React.js, Node/Express y MongoDB. Estoy muy interesado en aprender nuevas tecnologías para implementar en mis proyectos.</p>
+          <p className="text-textBlack text-lg md:text-xl text-center md:text-left font-normal content-end px-2 md:ps-0 md:pe-2 mt-4 md:ms-20 md:mb-20 xl:ms-72 xl:mb-20">Soy un apasionado desarrollador web especializado en Stack MERN con experiencia creando aplicaciones web con React.js, Node/Express y MongoDB. Estoy muy interesado en aprender nuevas tecnologías para implementar en mis proyectos.</p>
           <div className="w-full md:w-2/3 md:ms-20 xl:ms-56 px-4 xl:px-0 md:px-0 xl:flex justify-start">
             <a href={pdf} download className="w-full md:w-2/3 xl:w-2/4 md:ms-16 px-4 xl:px-0 mt-4 justify-center">
-              <Button pill outline className="cvBtn w-full border-4 border-reactLightBlue p-0 text-reactLightBlue font-bold">Descargar CV</Button>
+              <Button pill outline className="cvBtn w-full border-4 border-reactLightBlue p-0 text-reactLightBlue font-bold hover:bg-reactLightBlue ">Descargar CV</Button>
             </a>
             <Link className="hidden md:inline w-full md:w-2/3 xl:w-2/4 md:ms-16 xl:ms-10 px-4 mt-4 justify-center">
               <Button to="/contacto" pill className="contactoBtn w-full bg-reactLightBlue border-4 text-whiteBg font-bold">Contacto</Button>
