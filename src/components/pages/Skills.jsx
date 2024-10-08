12ea9b37fbd4c966aff3f4dff99ca551c4c6d4a0
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-08 15:17:52 -0300

    se agregan textos de páginas Skills

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,7 +1,13 @@
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <main className="min-h-[90vh] md:h-screen px-4 py-10">
+      <div className="flex flex-col justify-center items-center gap-4 my-4">
+        <h1 className="titulos text-reactLightBlue text-4xl font-bold">Habilidades</h1>
+        <p className="textos text-textBlack text-md">Estoy especializado en desarrollo web con HTML5, CSS3 y JavaScript junto con React.js. Entre otras herramientas y librerías que utilizo, se encuentran Bootstrap, Tailwind y una variada lista de librerías de React.</p>
+        <p className="textos text-textBlack text-md">Además tengo experiencia trabajando colaborativamente donde utilicé metodologías Agile como Scrum y Kanban, Trello y Jira para gestionar los proyectos y Git y GitHub como sistema de control de versiones.</p>
+      </div>
+    </main>
   )
 }
 
