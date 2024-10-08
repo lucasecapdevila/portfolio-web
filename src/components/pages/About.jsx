056b8bc7316b94765434c829e50b5dbf9bd26bf4
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-08 15:29:30 -0300

    se ajusta tamaño de imagen en página About

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="flex flex-col md:flex-row justify-center gap-4 mt-10">
           <img
             src={imagen}
-            className="rounded-lg md:w-2/4 lg:w-1/3 xl:w-1/4"
+            className="rounded-lg md:w-2/4 lg:w-96 xl:w-1/4"
             alt="lucas capdevila"
             data-aos="fade-right"
             data-aos-duration="1800"
