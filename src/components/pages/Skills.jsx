fb6b29ff4d98780e6ac5ab682e0ec2840149a0a6
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-09 18:36:35 -0300

    ajustes en tamaño de fuente

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -15,9 +15,9 @@ const Skills = () => {
   return (
     <main className="min-h-[90vh] max-w-[100vw] md:h-screen px-4 lg:ps-32 py-10">
       <div className="flex flex-col justify-center items-center gap-4 my-4">
-        <h1 className="titulos text-textDarkColor text-4xl font-bold">Habilidades</h1>
-        <p className="textos text-textBlack text-md">Estoy especializado en desarrollo web con HTML5, CSS3 y JavaScript junto con React.js. Entre otras herramientas y librerías que utilizo, se encuentran Bootstrap, Tailwind y una variada lista de librerías de React.</p>
-        <p className="textos text-textBlack text-md">Además cuento con experiencia trabajando colaborativamente donde utilicé metodologías Agile como Scrum y Kanban, Trello y Jira para gestionar los proyectos y Git y GitHub como sistema de control de versiones.</p>
+        <h1 className="titulos text-textDarkColor text-4xl lg:text-5xl font-bold">Habilidades</h1>
+        <p className="textos text-textBlack text-md lg:text-lg lg:mt-10">Estoy especializado en desarrollo web con HTML5, CSS3 y JavaScript junto con React.js. Entre otras herramientas y librerías que utilizo, se encuentran Bootstrap, Tailwind y una variada lista de librerías de React.</p>
+        <p className="textos text-textBlack text-md lg:text-lg">Además cuento con experiencia trabajando colaborativamente donde utilicé metodologías Agile como Scrum y Kanban, Trello y Jira para gestionar los proyectos y Git y GitHub como sistema de control de versiones.</p>
       </div>
       <div className="overflow-hidden whitespace-nowrap mt-20 lg:mt-72">
         <div className="inline-block animate-marquee space-x-10">
