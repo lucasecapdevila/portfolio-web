24e00bdd55c430a7b8faba694ef5f2dd0195df8f
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-10 20:03:54 -0300

    se agregan datos de proyectos

---
+++ src/utils/projectsData.js
@@ -0,0 +1,29 @@
+export const proyectos = [
+  {
+    id: 1,
+    titulo: "Portfolio Web",
+    descripcion: "Para este proyecto, diseñé y desarrollé mi portfolio web.",
+    repo: "https://github.com/lucasecapdevila/portfolio-web",
+    imagen: "../../public/projects/portfolioWeb.png",
+    tecnologias: ["React", "Tailwind CSS", "Flowbite"],
+    deploy: "https://lucasecapdevila.vercel.app/"
+  },
+  {
+    id: 2,
+    titulo: "Hotel Mirador del Valle",
+    descripcion: "Proyecto final para el curso de FullStack Developer de Rolling Code School realizado colaborativamente. Este proyecto es una página que sirve como sistema de gestión de un hotel que permite a los usuarios registrados reservar habitaciones disponibles y al administrador gestionar las habitaciones y usuarios.",
+    repo: "https://github.com/lucasecapdevila/hotel-mirador-del-valle-frontend",
+    imagen: "../../public/projects/hotelmiradordelvalle.png",
+    tecnologias: ["React", "Bootstrap", "Materialize", "Node", "Express", "MongoDB"],
+    deploy: "https://hotelmiradordelvalle.netlify.app/"
+  },
+  {
+    id: 3,
+    titulo: "Rolling Health",
+    descripcion: "Proyecto final para el curso de React Avanzado de Rolling Code School realizado colaborativamente. El objetivo de este proyecto fue desarrollar una plataforma integral de gestión para Rolling Health, un grupo de centros médicos de todo el país, diseñada para servir a tres tipos de usuarios: administradores, médicos y pacientes. Se puede realizar la gestión de citas, el historial médico de los pacientes, la administración de recursos del centro y la comunicación entre los diferentes usuarios.",
+    repo: "https://github.com/fagomezomil/rollinghealth",
+    imagen: "../../public/projects/rollinghealth.png",
+    tecnologias: ["React", "Tailwind CSS", "Zustand", "Node", "Express", "MongoDB"],
+    deploy: "https://rollinghealth.netlify.app/"
+  },
+]
