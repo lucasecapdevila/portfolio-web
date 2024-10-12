129d66c9a0bb7acf8870a7b7bf791a278fd8e4a4
Lucas Capdevila <lcapdevila60@gmail.com>
2024-10-12 20:09:05 -0300

    se ajusta la página para dispositivos lg

--- src/components/pages/projects/CardProject.jsx
+++ src/components/pages/projects/CardProject.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom"
 
 const CardProject = ({ proyecto }) => {
   return (
-    <Card className="projectCard max-w-sm">
-      <img src={proyecto.imagen} alt={proyecto.titulo} />
-      <h5 className="text-2xl font-bold tracking-tight text-textBlack">
+    <Card className="projectCard max-w-sm lg:max-w-md xl:max-w-lg">
+      <img src={proyecto.imagen} alt={proyecto.titulo}/>
+      <h5 className="titulos text-2xl font-bold tracking-tight text-textBlack">
         {proyecto.titulo}
       </h5>
-      <p className="font-normal text-textBlack">
+      <p className="textos font-normal text-textBlack">
         {proyecto.descripcion}
       </p>
       <ul className="flex flex-wrap">
         {proyecto.tecnologias.map((tecnologia) => {
-          return <li key={tecnologia} className="mr-2 mb-3 bg-gray-300 px-2 rounded">{tecnologia}</li>
+          return <li key={tecnologia} className="textos mr-2 mb-3 bg-gray-300 px-2 rounded">{tecnologia}</li>
         })}
       </ul>
-      <div className="flex justify-center items-center gap-4 md:mt-auto">
+      <div className="textos flex justify-center items-center gap-4 md:mt-auto">
         <Button className="w-full bg-textDarkColor">
           <Link to={proyecto.repo} target="_blank">GitHub</Link>
         </Button>
--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -7,20 +7,20 @@ import { Link } from "react-router-dom"
 
 const Projects = () => {
   return (
-    <main className="min-h-[90vh] md:h-screen px-4 pt-10 pb-5">
+    <main className="min-h-[90vh] min-w-full md:h-screen px-4 pt-10 pb-5 lg:ps-32">
       <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="text-textDarkColor text-4xl md:text-5xl font-bold">Proyectos</h1>
-        <p className="text-textBlack text-md md:px-8 md:text-lg">A continuación te presento algunos de los proyectos que he creado a partir de HTML y más recientemente, con React.</p>
+        <h1 className="titulos text-textDarkColor text-4xl md:text-5xl font-bold">Proyectos</h1>
+        <p className="textos text-textBlack text-md md:px-8 md:text-lg lg:mt-10">A continuación te presento algunos de los proyectos que he creado a partir de HTML y más recientemente, con React.</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4 mt-10">
         {proyectos.map((proyecto) => (
           <CardProject key={proyecto.id} proyecto={proyecto} />
         ))}
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center xl:justify-end xl:mt-5">
         <Link to='/' className="text-lg">
           <Button pill color={'success'} className="my-4 inline">
-            <p className="text-lg">Ver más proyectos</p>
+            <p className="textos text-whiteBg text-lg">Ver más proyectos</p>
             <span className="ml-2 text-lg">
               <FontAwesomeIcon icon={faArrowRight}/>
             </span>
