d7050b951495662c9bdda076997b155ff789b998
Lucas Capdevila <lcapdevila60@gmail.com>
2024-11-11 22:25:19 -0300

    se agrega traducciones a página de contacto

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,8 +3,11 @@ import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
 import toast, { Toaster } from "react-hot-toast";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation("global");
+
   const {
     register,
     reset,
@@ -34,45 +37,42 @@ const Contact = () => {
 
   return (
     <main className="min-h-[90vh] max-w-[100vw] md:h-screen px-4 lg:ps-32 py-10 lg:flex">
-      <div className="flex justify-center items-center gap-4 lg:mb-32">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-4 lg:mb-32">
         <div className="flex flex-col justify-center items-center gap-4 lg:w-1/2">
           <h1 className="titulos text-textDarkColor text-4xl md:text-5xl lg:self-start font-bold uppercase">
-            Contactame
+            {t("contact.title")}
           </h1>
           <h2 className="titulos text-textBlack text-2xl lg:text-3xl self-start">
-            ¿Quieres saber más de mí?
+            {t("contact.subtitle")}
           </h2>
           <p className="textos text-textBlack text-md lg:text-lg">
-            Estoy disponible para nuevas oportunidades y colaboraciones! Si estás
-            buscando alguien que pueda aportar nuevas ideas a tus proyectos y
-            obtener resultados significativos, no dudes en ponerte en contacto
-            conmigo.
+            {t("contact.text")}
           </p>
         </div>
 
         <form
-          className="textos flex max-w-md md:max-w-2xl flex-col gap-4 mt-10 border-2 rounded-md border-gray-300 p-4 md:mx-auto lg:w-full"
+          className="textos flex max-w-md md:max-w-2xl flex-col gap-4 mt-10 border-2 rounded-md border-gray-300 p-4 w-full"
           ref={form}
           onSubmit={handleSubmit(sendEmail)}
         >
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="name" value="Nombre" />
+              <Label htmlFor="name" value={t("contact.form_label_1")} />
             </div>
             <TextInput
               id="name"
               type="text"
-              placeholder="Ingresa tu nombre"
+              placeholder={t("contact.form_input_1")}
               shadow
               {...register("user_name", {
-                required: "El nombre es obligatorio.",
+                required: `${t("contact.form_input_1_required")}`,
                 minLength: {
                   value: 3,
-                  message: "El nombre debe tener al menos 3 caracteres",
+                  message: `${t("contact.form_input_1_minLength")}`,
                 },
                 pattern: {
-                  value: /^[a-zA-Z ]+$/gm,
-                  message: "El nombre no puede llevar carácteres ni números.",
+                  value: /^[A-zÀ-ú]+$/gm,
+                  message: `${t("contact.form_input_1_pattern")}`,
                 },
               })}
               helperText={
@@ -86,22 +86,22 @@ const Contact = () => {
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="email" value="Tu Email" />
+              <Label htmlFor="email" value={t("contact.form_label_2")} />
             </div>
             <TextInput
               id="email"
               type="email"
-              placeholder="Ingresa tu email"
+              placeholder={t("contact.form_input_2")}
               shadow
               {...register("user_email", {
-                required: "El email es obligatorio.",
+                required: `${t("contact.form_input_2_required")}`,
                 minLength: {
                   value: 4,
-                  message: "El email debe contener al menos 4 carácteres",
+                  message: `${t("contact.form_input_2_minLength")}`,
                 },
                 pattern: {
                   value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-                  message: "Ingrese una dirección de correo electrónico válida",
+                  message: `${t("contact.form_input_2_pattern")}`,
                 },
               })}
               helperText={
@@ -115,23 +115,22 @@ const Contact = () => {
           </div>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="message" value="Tu mensaje" />
+              <Label htmlFor="message" value={t("contact.form_label_3")} />
             </div>
             <Textarea
               id="message"
-              placeholder="Escribe tu mensaje..."
+              placeholder={t("contact.form_input_3")}
               rows={6}
               shadow
               {...register("message", {
-                required: "El mensaje es obligatorio.",
+                required: `${t("contact.form_input_3_required")}`,
                 minLength: {
                   value: 10,
-                  message: "El mensaje debe tener al menos 10 caracteres",
+                  message: `${t("contact.form_input_3_minLength")}`,
                 },
                 maxLength: {
-                  value: 200,
-                  message:
-                    "Debe ingresar como máximo 200 carácteres para la consulta.",
+                  value: 1000,
+                  message: `${t("contact.form_input_3_maxLength")}`,
                 },
               })}
               helperText={
@@ -143,8 +142,8 @@ const Contact = () => {
               }
             />
           </div>
-          <Button className="bg-textDarkColor" type="submit">
-            Enviar
+          <Button className="bg-textDarkColor lg:w-1/2 lg:self-end" type="submit">
+            {t("contact.button")}
           </Button>
         </form>
         <Toaster />
